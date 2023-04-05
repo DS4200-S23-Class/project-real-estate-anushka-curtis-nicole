9ac58f636b7be2a22897736db2a6a87ddde2283c
nv2000 <nicolevictory01@gmail.com>
2023-04-04 22:55:25 -0400

    scatter plot progress

--- src/components/charts/scatter.jsx
+++ src/components/charts/scatter.jsx
@@ -1,14 +1,63 @@
-import { ScatterChart, Scatter, XAxis, 
-    YAxis, CartesianGrid, Cell } from 'recharts';
+import {
+    ScatterChart, Scatter, XAxis,
+    YAxis, CartesianGrid, Cell, ResponsiveContainer, Tooltip
+} from 'recharts';
 
-export function ScatterPlot({value, city, attribute}) {
+export function ScatterPlot({ attribute, properties }) {
 
-    return (
-        <ScatterChart width={250} height={200}>
-                <CartesianGrid />
-            <XAxis type="number" dataKey= {value} />
-            <YAxis type="number" dataKey= {city} />
-            <Scatter data={attribute} fill="green" />
-        </ScatterChart>
-    )
+    const areaData = generateAreasData(attribute, properties)
+
+
+    return areaData.map((area) => <AreaScatterPlot name={area.area} data={area.data} />)
+
 };
+
+function AreaScatterPlot({ name, data }) {
+    return <ResponsiveContainer width="100%" height={60}>
+        <ScatterChart
+            width={800}
+            height={60}
+            margin={{
+                top: 10,
+                right: 0,
+                bottom: 0,
+                left: 0,
+            }}
+        >
+            <XAxis
+                type="category"
+                dataKey="hour"
+                name="hour"
+                interval={0}
+                tick={{ fontSize: 0 }}
+                tickLine={{ transform: 'translate(0, -6)' }}
+            />
+            <YAxis
+                type="number"
+                dataKey="index"
+                height={10}
+                width={80}
+                tick={false}
+                tickLine={false}
+                axisLine={false}
+                label={{ value: name, position: 'insideRight' }}
+            />
+            <Tooltip cursor={{ strokeDasharray: '3 3' }} wrapperStyle={{ zIndex: 100 }} content={this.renderTooltip} />
+            <Scatter data={data} fill="#8884d8" />
+        </ScatterChart>
+    </ResponsiveContainer>
+}
+
+const areas = ["Central", "Chappy", "Downtown", "East Chop", "Katama", "Lagoon",
+    "Lagoon/Harbor", "Lambert's Cove", "North", "Sengy", "South", "South Shore",
+    "State Beach", "Tashmoo", "West Chop"];
+
+function generateAreasData(attribute, properties) {
+    return areas.map(area => { return { area, data: mapAreaData(area, attribute, properties) } })
+}
+
+
+function mapAreaData(area, attribute, properties) {
+    const areaProperties = properties.filter(property => property.area === area);
+    return areaProperties.map((property) => { return property[attribute] })
+}
--- src/pages/map.jsx
+++ src/pages/map.jsx
@@ -58,14 +58,12 @@ function getMax(attribute, collection) {
 
 function getAllValues(attribute, collection) {
   const list = []
-  for(attribute in collection) {
+  for (attribute in collection) {
     return list.push(attribute.value);
   }
 }
 
-const listOfCityNames = ["Central","Chappy,Downtown","East Chop","Katama","Lagoon",
-"Lagoon/Harbor","Lambert's Cove","North","Sengy","South","South Shore", 
-"State Beach", "Tashmoo", "West Chop"];
+
 
 export default function MapPage({ properties }) {
   const [selectedProperty, setSelectedProperty] = useState({});
@@ -104,16 +102,15 @@ export default function MapPage({ properties }) {
                       )}
                       {key.type === 'range' && (
                         <div className="pt-2 pb-6">
-                        <ScatterPlot 
-                        value = {selectedProperty[key.name]}
-                        city = {selectedProperty.area}
-                        attribute = {properties[key.name]} />
-                        
-                         <LineChart
+                          <ScatterPlot
+                            properties={properties}
+                            attribute={key.name} />
+
+                          <LineChart
                             min={getMin(key.name, properties)}
                             max={getMax(key.name, properties)}
                             value={selectedProperty[key.name]}
-                            formatter={key.formatter} 
+                            formatter={key.formatter}
                           />
                         </div>
                       )}
